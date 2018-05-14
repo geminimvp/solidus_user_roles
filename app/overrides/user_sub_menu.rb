db9c5634a7264b38a3811d1d924164e5eeeb7692
Carlos Andres Herrera <carlos@nimbl3.com>
2018-05-15 05:38:10 +0800

    Revert "Adapting the menu item to the new UI"

--- app/overrides/user_sub_menu.rb
+++ app/overrides/user_sub_menu.rb
@@ -5,12 +5,7 @@ Deface::Override.new(
 ) do
   <<-HTML
     <% if can? :admin, Spree::Role %>
-      <li class="menu-item <%=  mark_as_active(spree.admin_roles_path, 'active')  %>">
-        <%= link_to spree.admin_roles_path do %>
-          <i class="icon menu-icon menu-icon-users mr-2"></i>
-          <%= Spree.t(:roles, scope: [:admin, :tab]) %>
-        <% end %>
-      </li>    
+      <%= tab(:roles, icon: 'users') %>
     <% end %>
   HTML
 end
