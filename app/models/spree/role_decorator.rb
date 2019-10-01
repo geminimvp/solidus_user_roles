b6097fae7584476604cbca12ca07b60c9cb448b8
fastjames <fastjames@gmail.com>
2019-10-01 10:50:24 -0500

    Tolerate missing permission set classes
    In some cases, a permission set class that is referred to in a role
    definition may be removed. We need to tolerate that absence so that the
    application can start normally.

--- app/models/spree/role_decorator.rb
+++ app/models/spree/role_decorator.rb
@@ -1,22 +1,34 @@
-Spree::Role.class_eval do
-  has_many :role_permissions, dependent: :destroy
-  has_many :permission_sets, through: :role_permissions
+module Spree
+  module RoleDecorator
+    def self.prepended(base)
+      base.has_many :role_permissions, dependent: :destroy
+      base.has_many :permission_sets, through: :role_permissions
 
-  scope :non_base_roles, -> { where.not(name: ['admin', 'user']) }
-  validates :name, uniqueness: true
-  after_save :assign_permissions
-
-  def permission_sets_constantized
-    permission_sets.map(&:set).map(&:constantize)
-  end
-
-  def assign_permissions
-    if SolidusSupport.solidus_gem_version < Gem::Version.new('2.5.x')
-      Spree::RoleConfiguration.configure do |config|
-        config.assign_permissions name, permission_sets_constantized
-      end
-    else
-      Spree::Config.roles.assign_permissions name, permission_sets_constantized
+      base.scope :non_base_roles, -> { where.not(name: ['admin', 'user']) }
+      base.validates :name, uniqueness: true
+      base.after_save :assign_permissions
     end
+
+    def permission_sets_constantized
+      permission_sets.map(&:set).map { |set_class_name|
+        begin
+          set_class_name.constantize
+        rescue NameError
+          nil
+        end
+      }.compact
+    end
+
+    def assign_permissions
+      if SolidusSupport.solidus_gem_version < Gem::Version.new('2.5.x')
+        Spree::RoleConfiguration.configure do |config|
+          config.assign_permissions name, permission_sets_constantized
+        end
+      else
+        Spree::Config.roles.assign_permissions name, permission_sets_constantized
+      end
+    end
+
+    Spree::Role.prepend(self)
   end
 end
